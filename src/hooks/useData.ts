87b64422b67b30c9cfa4704d85b72aa501b6ac20
AndreiaMsT <am.teixeira4@gmail.com>
2023-11-26 17:08:44 +0000

    Display genres

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
@@ -9,6 +8,7 @@ interface FetchResponse<T> {
     count: number;
     results: T[];
   }
+  
 
 const useData = <T> (endpoint:string) => {
   const [data, setData] = useState<T[]>([]);
